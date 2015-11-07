f1e6f8dfd727ade6ffd46330b25de3aa92c4ae94
karlpatrickespiritu <wiwa.espiritu@gmail.com>
2015-11-08 00:05:46 +0800

    plan

--- src/object/object.js
+++ src/object/object.js
@@ -5,28 +5,37 @@
             $: function (data) {
                 return {};
             }
-        }tpouctouc
+        }
     })()
 
+    // boolean
+    _.is(somevalue).defined()
+    _.is(somevalue).aString()
+    _.is(somevalue).aNumber()
+    _.is(somevalue).aBoolean()
+    _.is(somevalue).aFuncion()
+    _.is(somevalue).anObject()
+    _.is(somevalue).anArray()
+    _.is(somevalue).true()
+    _.is(somevalue).false()
+    _.is(somevalue).aPropertyOf(object)
 
-    _.isDefined()
-    _.isString()
-    _.isNumber()
-    _.isNull()
-    _.isArray()
-    _.isObject()
-    _.isFunction()
+    // arrays
+    _.does('john').existIn(['john', 'james'])
+    _.add('john').to(['james'])
+    _.remove('james').in(['james', 'john'])
+    _.getLastItem([1, 2, 3])
+    _.getFirst([1, 2, 3])
 
-    _.$('john').exists(['john', 'james'])
-    _.$('hi').upperCaseFirstLetter()
-    _.$('hi, i am john.').upperCaseFirstLetters()
-    _.$('notcamelcased').toCamelCase()
+    // strings
+    _.convert('hi').toUpperCaseFirstLetters()
+    _.convert('hi, i am john\.').toUpperCaseFirstLetters()
+    _.convert('notcamelcased').toCamelCase()
 
 
-    _.$(['john', 'doe']).each(function (index, value) {
-        if (_.$(value).exist(someArray)) {
+    // loops
+    _.forEvery([1, 2, 3], function(i, v) {
 
-        }
     })
 
 })()
